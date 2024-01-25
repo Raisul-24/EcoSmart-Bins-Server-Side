161bc3459ff256d5ffe9d151b906a3c333a108bc
MD SAMIT INTESAR <saadsamit64@gmail.com>
2024-01-25 14:18:48 +0600

    add update api service route

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 8085;
 // middleware
-app.use(cors({
-  origin: [
-    "http://localhost:5173"
-  ],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.axstdh0.mongodb.net/?retryWrites=true&w=majority`;
@@ -53,15 +53,31 @@ const dbConnect = async () => {
     //service add new data
     app.post("/services", async (res, req) => {
       const data = res.body;
-      const addData = await services.insertOne(data)
+      const addData = await services.insertOne(data);
       req.send(addData);
     });
     //delete a service
     app.delete("/services/:id", async (res, req) => {
       const id = res.params.id;
       const query = { _id: new ObjectId(id) };
-      const deleteData = await services.deleteOne(query)
+      const deleteData = await services.deleteOne(query);
       req.send(deleteData);
+    });
+    //update a service
+    app.patch("/services/:id", async (res, req) => {
+      const id = res.params.id;
+      const query = { _id: new ObjectId(id) };
+      const data = res.body;
+      const updateDoc = {
+        $set: {
+          img: data.img,
+          title: data.title,
+          drescaption: data.drescaption,
+        },
+      };
+      const options = { upsert: true };
+      const updateData = await services.updateOne(query, updateDoc, options);
+      req.send(updateData);
     });
 
     console.log("DB Connected Successfully✅");
