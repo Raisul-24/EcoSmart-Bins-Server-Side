69ad880a3a7bd039d8f7e412534b724ca9fa05ca
Coder Mahdi <codermahdihasan@gmail.com>
2024-01-30 19:50:33 +0600

    added middlewares for token verify

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+//middlewares for token verify
+const verifyToken = (req, res, next) => {
+  console.log("inside the verifyToken", req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 const dbConnect = async () => {
   try {
     //  await client.connect();
@@ -38,7 +55,7 @@ const dbConnect = async () => {
 
     app.get("/my-cart", async (req, res) => {
       try {
-        const query = req.query;
+        const query = req.body;
         const result = await myCart.find(query).toArray();
         //console.log(result);
         res.json(result);
